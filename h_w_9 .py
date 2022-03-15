b16c4f99ee451e03fd2805fb476a2b98399b8088
dimskay1988@icloud.com <Dima5063638>
2022-03-15 14:00:12 +0300

    Переименовал из "passengerCar" в "PassengerCar" Переименовал из "getСount" в "get_count"

--- h_w_9 .py
+++ h_w_9 .py
@@ -1,27 +1,27 @@
-class passengerCar():
+class PassengerCar():
 
     _count = 0
 
     def info(self):
         print('Vehicle up to 3.5 tons and up to 8 passengers')
 
-class Car(passengerCar):
+class Car(PassengerCar):
 
     def __init__(self, weight: int = 0,  name: str = 'no name', model: str = 'no name', age: int = 1900):
-        passengerCar._count += 1
+        PassengerCar._count += 1
         self.weight = weight
         self.name = name
         self.model = model
         self.age = age
 
     @staticmethod
-    def getCount():
+    def get_count():
         return Car._count
 
     def info_car(self):
         print('My', self.name, self.model, 'has', self.age, 'age')
 
-    def foo(passengerCar):
+    def foo(PassengerCar):
         super().info()
         print('Freight car')
 
@@ -37,4 +37,4 @@ bmw.info_car()
 audi.metod()
 
 audi.foo()
-print(f'Number of objects created: {Car.getCount()}')
+print(f'Number of objects created: {Car.get_count()}')
