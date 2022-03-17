54258221249596df4e250e02dbb49304d5932110
dimskay1988@icloud.com <Dima5063638>
2022-03-17 16:47:24 +0300

    Переименовал num1 в num_1, num2 в num_2. Обернул в функцию калькулятор. Добавил пустую строку.

--- h_w_10.py
+++ h_w_10.py
@@ -1,30 +1,35 @@
-while True:
-    s = input('Введите действие (+,-,/,*): ')
-    d = ('+', '-', '/', '*')
-    if s in d:
-        print('Пока все верно')
-    else:
-        print('Неверный знак операции!')
-        continue
-    try:
-        num1 = float(input('Введите первое число: '))
-        num2 = float(input('Введите второе число: '))
-    except ValueError:
-        print('Пожалуйста, вводите только числа')
-    else:
-        if s == '+':
-            print(num1 + num2)
-        elif s == '-':
-            print(num1 - num2)
-        elif s == '*':
-            print(num1 * num2)
-        elif s == '/':
-            try:
-                if num2 == 0:
-                    print(num1 / num2)
-            except ZeroDivisionError:
-                print('Делить на ноль нельзя')
-            else:
-                print(num1 / num2)
-    if input('Если не хотите продолжить введите "n": ') == 'n':
-        break
+def calculator(s: str, num1: float, num2: float) -> float:
+    """Простейший калькулятор"""
+    while True:
+        s = input('Введите действие (+,-,/,*): ')
+        d = ('+', '-', '/', '*')
+        if s in d:
+            print('Пока все верно')
+        else:
+            print('Неверный знак операции!')
+            continue
+        try:
+            num_1 = float(input('Введите первое число: '))
+            num_2 = float(input('Введите второе число: '))
+        except ValueError:
+            print('Пожалуйста, вводите только числа')
+        else:
+            if s == '+':
+                print(num_1 + num_2)
+            elif s == '-':
+                print(num_1 - num_2)
+            elif s == '*':
+                print(num_1 * num_2)
+            elif s == '/':
+                try:
+                    if num2 == 0:
+                        print(num_1 / num_2)
+                except ZeroDivisionError:
+                    print('Делить на ноль нельзя')
+                else:
+                    print(num_1 / num_2)
+        if input('Если не хотите продолжить введите "n": ') == 'n':
+            break
+
+calculator()
+
