3946907dd2717f136868bbefad9716bb177a02b5
Dima Anikeenko <100229615+Dimskay1988@users.noreply.github.com>
2022-03-27 17:51:42 +0300

    Delete h_w_11.py

--- .idea/h_w_11.py
+++
@@ -1,9 +0,0 @@
-def GeneratorG_P(b1: int, q: int, n: int) -> int:
-    """Генератор геометрической прогрессии"""
-    count = 1
-    while count <= n:
-        yield (b1 * q ** (count - 1))
-        count += 1
-
-for num in GeneratorG_P(1, 2, 10):
-    print(num)
